dab45567271c19e703985d8a2ea272f88b17ea83
seongminyoo <dbtjdals1771@ajou.ac.kr>
2020-08-08 22:05:38 +0900

    test commit 4

--- git.sh
+++ git.sh
@@ -1,6 +1,21 @@
+echo "############################################"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#    git pull push auto script ver 1.0     #"
+echo "#                                          #"
+echo "#         written by sungminyou            #"
+echo "#                                          #"
+echo "#                                          #"
+echo "#                                          #"
+echo "############################################"
+echo "\n"
+echo "start pull"
 git pull
-echo "커밋 메시지를 입력하세요"
+echo "please enter a commit message"
 read line
 git add .
 git commit -am "$line"
 git push
+echo "Pushing ended successfully"
